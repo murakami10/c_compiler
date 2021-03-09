804de9975226d8477184b2f708faa24f7090f6d7
kyoya <m.kyoya777@gmail.com>
2021-03-10 08:33:54 +0900

    単項演算子を追加

--- test.sh
+++ test.sh
@@ -16,9 +16,22 @@ assert(){
     fi
 }
 
+# 入力された数字を出力
 assert 0 0
 assert 42 42
+
+# 文字列での加减の追加
 assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5"
+
+# 乗除と()を追加
+assert 47 "5+6*7"
+assert 15 "5*(9-6)"
+assert 4  "(3+5)/2"
+
+# 単項演算子
+assert 10 "-10+20"
+assert 10 "- -10"
+assert 10 "- - + 10"
 
 echo OK
